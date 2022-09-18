ad8caa22e7b13b3e1a3a2d93b61a80dbd06a8a24
Edmilson Junior <jredmilsonn@outlook.com>
2022-09-18 01:24:47 -0300

    Starting a developer tabuleiro

--- Program.cs
+++ Program.cs
@@ -8,11 +8,9 @@ namespace Xadrez_Console
         static void Main(string[] args)
         {
 
-            Posicao P;
-
-            P = new Posicao(3, 4);
-
-
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            
+            Console.WriteLine(tab);
         }
     }
 }
---
+++ Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        public Tabuleiro tab { get; protected set; }
+
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.tab = tab;
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+    }
+}
